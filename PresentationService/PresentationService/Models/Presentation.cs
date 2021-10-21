c78a29a2d3f83532e29bd54428122953c45fd373
Александр Овчаров <ovcharov.a@dobrozaim.ru>
2021-10-21 14:27:11 +0300

    First Step

---
+++ PresentationService/PresentationService/Models/Presentation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationService.Models
+{
+    public class Presentation
+    {
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public StatusPresentation Status { get; set; }
+        public string Description { get; set; }
+        public List<Visitor> visitors = new List<Visitor>();
+    }
+
+    public enum StatusPresentation
+    {
+        Open,
+        Close
+    }
+}
